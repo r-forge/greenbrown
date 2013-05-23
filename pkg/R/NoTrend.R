4b3eb60204d33775b6f193c9201f56f4ead13e3d
mforkel (Matthias Forkel) <matthias.forkel@tu-dresden.de>
2013-05-23 06:25:44 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/greenbrown@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/NoTrend.R
+++ pkg/R/NoTrend.R
@@ -2,11 +2,12 @@ NoTrend <- structure(function(
 	##title<<
 	## Initialize an empty list of trends
 	
-	##description<< This is an internal function to make an empty list of breakpoints. For the user there is usually no need to use this function.
+	##description<< This is an internal function to make an empty list of class Trend. For the user there is usually no need to use this function.
 	
 	Yt
 	### univariate time series of class \code{\link{ts}}
 ) {
+	bptest <- list(statistic=NA, p.value=NA)
 	result <- list(
 		series = rep(0, length(Yt)),
 		trend = rep(0, length(Yt)),
@@ -14,7 +15,7 @@ NoTrend <- structure(function(
 		bp = NoBP(),
 		slope = NA,
 		pval = NA,
-		bptest = NA,
+		bptest = bptest,
 		method = "AAT")
 	class(result) <- "Trend"
 	return(result)
