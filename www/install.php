a372b6cb77bff706ed938ee26bb46cdc27f87b5e
mforkel (Matthias Forkel) <matthias.forkel@tu-dresden.de>
2015-06-02 11:07:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/greenbrown@53 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/install.php
+++ www/install.php
@@ -51,18 +51,35 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <h2>Installation and loading</h2>
 
-<p>The package greenbrown was developed since the R version 2.15.3. To install the package directly from Rforge you need the most recent R version.</p>
+<p>The package greenbrown was developed since the R version 2.15.3. To install the package directly from R-forge you need the most recent R version.</p>
 
 <p>To install the most recent version of greenbrown type directly within R: <br><b>install.packages("greenbrown", repos="http://R-Forge.R-project.org")</b></p>
 
 <p>If you want to use greenbrown, you have to load the package at the beginning of each R session with: <br><b>library(greenbrown)</b></p> 
 
-<p>Alternatively, you can receive all greenbrown source code files by using an anonymous SVN checout: <br><b>
-svn checkout svn://r-forge.r-project.org/svnroot/greenbrown/</b></p>
+
+<h3>Problems during installation</h3>
+
+<p>You get during the installation a warning like this one?</p>
+
+<p><b>Warning message:<br>
+package 'greenbrown' is not available (for R version X.Y.Z) </b></p>
+
+<p>This message indicates that the most recent version or any updates to the greenbrown package were not yet build to a package.  R-forge is a development platform and greenbrown is continously developed. Therefore it can often happen that the package needs to be checked and re-build. During this stage the package cannot be directly installed with <b>install.packages</b>. However if you still want to use the package, you can receive the most recent functions through a svn checkout:</p>
+
+<p><b>svn checkout svn://r-forge.r-project.org/svnroot/greenbrown/</b></p>
+
+<p>In the next step you can just source the files directly within R:</p>
+
+<p>
+<b>setwd("yourdirectory/greenbrown/R/")<br>
+files <- list.files(pattern=".R")<br>
+for (i in 1:length(files)) source(files[i])</b>
+
 
 <br>
 <hr>
-<p><a href="index.php">greenbrown</a>, Matthias Forkel, mforkel [at] bgc-jena.mpg.de, 2015-04-15</p>
+<p><a href="index.php">greenbrown</a>, Matthias Forkel, mforkel [at] bgc-jena.mpg.de, 2015-06-02</p>
 
 </body>
 </html>
