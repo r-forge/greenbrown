39c1341d8445ee61b268c10786751621559d468c
mforkel (Matthias Forkel) <matthias.forkel@tu-dresden.de>
2016-11-18 07:13:56 +0000

    SOfireA: update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/greenbrown@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/SOfireA/R/SofiaLogistic.R
@@ -0,0 +1,43 @@
+SofiaLogistic <- structure(function(
+	##title<< 
+	## Logistic function 
+	##description<<
+	## Compute values of a logistic function as used in Sofia models.
+	
+	par, 
+	### parameters of logistic function, a vector of length 3 (asymptote, slope, turning point)
+	
+	x, 
+	### independent variable
+   
+   ...
+   ### further arguments (not used)
+
+	##details<<
+	## No details.
+	
+	##references<< No reference.	
+	
+	##seealso<<
+	## \code{\link{Sofia}}
+) { 
+   y <- par[1] / (1 + exp(-par[2] * (x - par[3])))
+   y[y > 1] <- 1
+   return(y)
+   ### a vector
+}, ex=function() {
+x <- -20:20
+par <- c(1, 0.5, 0)
+plot(x, SofiaLogistic(par, x), type="l")
+
+par <- c(1, 0.2, 0)
+plot(x, SofiaLogistic(par, x), type="l")
+
+par <- c(10, -1, 0)
+plot(x, SofiaLogistic(par, x), type="l")
+
+par <- c(-2, -1, 0)
+plot(x, SofiaLogistic(par, x), type="l")
+
+})
+
